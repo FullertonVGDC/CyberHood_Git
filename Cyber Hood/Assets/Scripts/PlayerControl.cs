947541ce2205329983b7c8ee0f3631bee11fd451
DJDomingo <djdomingo71@gmail.com>
2019-09-30 00:46:18 -0700

    Rotation Slerp
    Camera now rotates smoothly around player and can only be rotated once current rotation is complete.

--- Cyber Hood/Assets/Scripts/PlayerControl.cs
+++ Cyber Hood/Assets/Scripts/PlayerControl.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public int playerSpeed = 5;
-    public int camSlerpAmt = 5;
+
+    //Vars for Camera Rotation:
+    bool changeCamAngle = false;
+    float timeTakenDuringSlerp = 1f;
+    Quaternion q_startPos;
+    Quaternion q_endPos;
+    Vector3 tempEuler;
+    float timeStartedSlerp;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +28,32 @@ public class PlayerControl : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) { transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime); }
         if (Input.GetKey(KeyCode.S)) { transform.Translate(Vector3.back * playerSpeed * Time.deltaTime); }
 
-        if (Input.GetKeyDown(KeyCode.E)) { transform.Rotate(0, 90, 0); }
-        if (Input.GetKeyDown(KeyCode.Q)) { transform.Rotate(0, -90, 0); }
+        //if (Input.GetKeyDown(KeyCode.E)) { transform.Rotate(0, 90, 0); }
+        //if (Input.GetKeyDown(KeyCode.Q)) { transform.Rotate(0, -90, 0); }
 
+        if (Input.GetKeyDown(KeyCode.E) && !changeCamAngle) { StartSlerp(90); }
+        if (Input.GetKeyDown(KeyCode.Q) && !changeCamAngle) { StartSlerp(-90); }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (changeCamAngle) {
+            float timeSinceStarted = Time.time - timeStartedSlerp;
+            float percComplete = timeSinceStarted / timeTakenDuringSlerp;
+            transform.rotation = Quaternion.Slerp(q_startPos, q_endPos, percComplete);
+            if (percComplete >= 1.0f)
+                changeCamAngle = false;
+        }
+    }
+
+
+    void StartSlerp(int angleChange) {
+        changeCamAngle = true;
+        timeStartedSlerp = Time.time;
+        q_startPos = transform.rotation;
+        tempEuler = new Vector3(q_startPos.eulerAngles.x, q_startPos.eulerAngles.y+angleChange, q_startPos.eulerAngles.z);
+        q_endPos = Quaternion.Euler(tempEuler);
     }
 }
