831a90bbdfcee39791b1f9c23435117e0fefb775
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2019-10-02 22:50:08 -0700

    Added Basic Enemy & Scripted Its Movement

---
+++ Cyber Hood/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour {
+
+    public Transform[] path;
+    public float speed;
+
+    private Rigidbody body;
+    private int currentIndex;
+    private int nextIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+
+        nextIndex = GetIndexToClosestPoint();
+        currentIndex = (nextIndex - 1) % path.Length;
+    }
+
+    int GetIndexToClosestPoint()
+    {
+        float minDistance = Mathf.Infinity;
+        int index = -1;
+        for (int i = 0; i < path.Length; i++)
+        {
+            Vector3 position = path[i].position;
+
+            if (minDistance > Vector3.Distance(transform.position, position))
+            {
+                minDistance = Vector3.Distance(transform.position, position);
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, path[nextIndex].position) >= 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, path[nextIndex].position, speed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            currentIndex = nextIndex;
+            nextIndex = (currentIndex + 1) % path.Length;
+        }
+    }
+}
--- README.txt
+++ README.txt
@@ -5,3 +5,4 @@ Updates:
 9/28/19 - Unity Project Created and uploaded to Git
 10/1/19 - Sample Scene created with player.
 	Player movement and basic fncs such as health and collision have been added
+10/2/19 - Created basic movement for AI
