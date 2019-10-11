e6e1801e3bfc5d8604fb4133c5afadf63bd9189e
Ean McGilvery <eanmcgilvery@gmail.com>
2019-10-11 10:45:45 -0700

    added the door

---
+++ Cyber Hood/Assets/Scripts/Door/Sliding_door_script.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Sliding_door_script : MonoBehaviour
+{
+
+    public GameObject Trigger;
+    public GameObject door;
+
+    Animator doorAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnim = door.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SlideDoor(bool state)
+    {
+        doorAnim.SetBool("slide", state);
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            SlideDoor(true);
+        }
+
+    }
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            SlideDoor(false);
+        }
+    }
+
+ 
+}
