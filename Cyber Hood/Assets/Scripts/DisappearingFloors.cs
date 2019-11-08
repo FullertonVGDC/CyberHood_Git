831cd3c8ee3fd7d3a4571e18209b8b5dca0fa270
Daniel Domingo <djdomingo71@gmail.com>
2019-11-08 15:35:09 -0800

    Disappearing Floors
    Added floors that disappear every few seconds. Can be set via Inspector

---
+++ Cyber Hood/Assets/Scripts/DisappearingFloors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class DisappearingFloors : MonoBehaviour
+{
+    bool isChanging = false;
+    bool ActiveStatus = true;
+    public int timerAmt;
+
+    void Update()
+    {
+        if (!isChanging) {
+            isChanging = true;
+            StartCoroutine(ChangeActive());
+        }
+    }
+
+
+    IEnumerator ChangeActive() {
+        ActiveStatus = !ActiveStatus;
+        this.gameObject.GetComponent<MeshRenderer>().enabled = ActiveStatus;
+        this.gameObject.GetComponent<BoxCollider>().enabled = ActiveStatus;
+
+        yield return new WaitForSeconds(timerAmt);
+        Debug.Log("HALP!");
+        isChanging = false;
+        yield return null;
+    }
+
+}
